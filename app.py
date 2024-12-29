1d3695e867b441559d779e99a04a92548c6e77ab
mametaro99 <bearsmichimato99@gmail.com>
2024-12-29 21:59:39 +0900

    カメラチェックを実装

--- app.py
+++ app.py
@@ -8,6 +8,35 @@ import numpy as np
 arrow_length = 50
 arrow_color = (0, 255, 0)  # 矢印の色を設定（BGR形式）
 
+
+def check_camera_connection():
+    """
+    接続されているカメラをチェック
+    """
+
+    print('接続されているカメラの番号を調べています...')
+    true_camera_is = []  # 空の配列を用意
+    cam_number = []
+
+    # カメラ番号を0～9まで変えて、COM_PORTに認識されているカメラを探す
+    for camera_number in range(0, 10):
+        try:
+            cap = cv2.VideoCapture(camera_number)
+            ret, frame = cap.read()
+        except:
+            ret = False
+        if ret == True:
+            true_camera_is.append(camera_number)
+            print("カメラ番号->", camera_number, "接続済")
+
+            cam_number.append(camera_number)
+        else:
+            print("カメラ番号->", camera_number, "未接続")
+    print("接続されているカメラは", len(true_camera_is), "台です。")
+    print("カメラのインデックスは", true_camera_is,"です。")
+    sys.exit("カメラ番号を調べ終わりました。")
+    return 0
+
 # モデルロード
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(
@@ -57,7 +86,8 @@ def draw_gaze_arrow(image, eye_center, iris_center, direction, arrow_length):
     return image
 
 if __name__ == '__main__':
-    cap = cv.VideoCapture(0)  # Webカメラをキャプチャ
+    check_camera_connection()
+    cap = cv.VideoCapture(1)  # Webカメラをキャプチャ
 
     while True:
         ret, image = cap.read()
